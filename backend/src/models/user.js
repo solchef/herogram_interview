eb9e93783c625b4dc2da003c7a4aa66b3ab53078
jchege <jawiwy@gmail.com>
2024-11-01 19:27:51 +0400

    error validation for register

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true }, // Ensure this is unique
+    username: { type: String, required: true, unique: false }, // Ensure this is unique
     name: { type: String, required: true },
     password: { type: String, required: true }
 }, { timestamps: true });
